594419c4c08b9421e802f206c069887359daacfe
Srinivasa Rao Sadineni <sadinenisrinivasarao6@gmail.com>
2025-07-16 10:34:09 -0500

    figma update

--- js/app.js
+++ js/app.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content, .block-2', {
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['FullStack Developer', 'Frontend Developer', 'React Developer', 'Web Developer'],
+    strings: ['FullStack Developer', 'Frontend Developer', 'React Developer', 'Web Developer', 'UI/UX Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
