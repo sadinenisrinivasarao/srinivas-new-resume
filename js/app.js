7af519123801fcc8fd21b2c6f109e2226e1d7576
Srinivasa Rao Sadineni <sadinenisrinivasarao6@gmail.com>
2025-07-18 22:23:39 -0500

    figma portfolio update

--- js/app.js
+++ js/app.js
@@ -66,6 +66,30 @@ const typed = new Typed('.multiple-text', {
     loop: true
 });
 
+// JavaScript to control button behavior
+const uxBtn = document.getElementById("uxBtn");
+const uxLink = uxBtn.querySelector("a");
+
+function updateButtonBehavior() {
+  if (window.innerWidth < 1024) {
+    uxBtn.classList.add("disabled");
+    uxBtn.textContent = "Can only access in laptop";
+   
+  } else {
+    // Enable link for desktop
+    uxBtn.classList.remove("disabled");
+    uxLink.setAttribute("href", "https://harley-wmr2g74hc9.figweb.site/desktop/");
+    uxBtn.textContent = "View my UX Portfolio Design";
+  }
+}
+
+
+// Run on load
+updateButtonBehavior();
+
+// Run on resize
+window.addEventListener("resize", updateButtonBehavior);
+
 
 function adjustBoxHeights() {
 
